0ac8e528ad2ee8b8fe658fc599f29be13c6d65fa
Amos Feng <zfeng@redhat.com>
2018-10-25 22:04:34 +0800

    update to add the destroy method

--- src/main/java/io/shardingsphere/transaction/manager/xa/narayana/NarayanaTransactionManager.java
+++ src/main/java/io/shardingsphere/transaction/manager/xa/narayana/NarayanaTransactionManager.java
@@ -1,33 +1,29 @@
 /*
- * JBoss, Home of Professional Open Source.
- * Copyright 2018, Red Hat, Inc., and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
+ * Copyright 2018 Red Hat, Inc, and individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package io.shardingsphere.transaction.manager.xa.narayana;
 
+import com.arjuna.ats.arjuna.recovery.RecoveryManager;
 import com.arjuna.ats.internal.jta.recovery.arjunacore.XARecoveryModule;
+import com.arjuna.ats.jbossatx.jta.RecoveryManagerService;
 import com.arjuna.ats.jta.common.jtaPropertyManager;
 import com.arjuna.ats.jta.recovery.XAResourceRecoveryHelper;
 import io.shardingsphere.core.event.transaction.xa.XATransactionEvent;
 import io.shardingsphere.core.rule.DataSourceParameter;
 import io.shardingsphere.transaction.manager.xa.XATransactionManager;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.sql.DataSource;
 import javax.sql.XADataSource;
@@ -37,11 +33,29 @@ import java.sql.SQLException;
 /**
  * @author zhfeng
  */
+@Slf4j
 public class NarayanaTransactionManager implements XATransactionManager {
     private static final UserTransaction USER_TRANSACTION_MANAGER = jtaPropertyManager.getJTAEnvironmentBean().getUserTransaction();
     private static final XARecoveryModule xaRecoveryModule =  XARecoveryModule.getRegisteredXARecoveryModule();
+    private static final RecoveryManagerService recoveryManagerService = new RecoveryManagerService();
+
+
+    public NarayanaTransactionManager () {
+        RecoveryManager.delayRecoveryManagerThread();
+        recoveryManagerService.create();
+        recoveryManagerService.start();
+    }
 
     @Override
+    public void destroy() {
+        try {
+            recoveryManagerService.stop();
+        } catch (Exception e) {
+            log.warn("stop recoveryManagerService failed with " + e);
+        }
+        recoveryManagerService.destroy();
+    }
+
     public DataSource wrapDataSource(XADataSource dataSource, String dataSourceName, DataSourceParameter dataSourceParameter) throws Exception {
         DataSource ds = new NarayanaDataSource(dataSource);
         xaRecoveryModule.addXAResourceRecoveryHelper(getRecoveryHelper(dataSource, dataSourceParameter));
